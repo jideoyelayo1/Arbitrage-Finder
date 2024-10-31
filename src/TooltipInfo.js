e09bcebc31f8c31cc2c232d36dc5bb44e0d26527
Jide Oyelayo <jideoyelayo1@gmail.com>
2024-10-31 15:05:35 +0000

    multi stall betting explained

--- src/TooltipInfo.js
+++ src/TooltipInfo.js
@@ -4,6 +4,7 @@ function TooltipInfo() {
   return (
     <div>
       This calculator helps you find arbitrage opportunities by entering different outcomes and their odds.
+      When betting across multiple markets, put the best odds for each outcome.
     </div>
   );
 }
